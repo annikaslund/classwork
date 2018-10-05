5eb3091480b93fd7db315d61dbceacf198269078
annika <annikaslund@gmail.com>
2018-10-05 09:45:33 -0700

    Complete Data Structures Phase 1: Implement a PolyTreeNode class

---
+++ poly_tree_node/skeleton/lib/00_tree_node.rb
@@ -0,0 +1,26 @@
+class PolyTreeNode
+  attr_accessor :value, :parent, :children
+
+  def initialize(value)
+    @value = value
+    @parent = nil
+    @children = []
+  end
+
+  def parent
+    @parent
+  end
+
+  def children
+    @children
+  end
+
+  def value
+    @value
+  end
+
+  def parent=(node)
+    @parent = node
+    node.children << self
+  end
+end
